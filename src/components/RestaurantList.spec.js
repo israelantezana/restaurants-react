b85f7a7b3b6670288a21f47e02d87d37aa12238b
Israel Antezana Rojas <israelantezana@gmail.com>
2022-09-28 21:53:16 -0400

    Muestra los restaurants, toma en cuenta errores del API. Pruebas pasando con MSW

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -1,30 +1,53 @@
-import {render, screen} from '@testing-library/react';
+/* eslint-disable jest/valid-expect */
+/* eslint-disable testing-library/no-wait-for-multiple-assertions */
+/* eslint-disable no-unused-expressions */
+import {render, screen, waitFor} from '@testing-library/react';
 import RestaurantList from './RestaurantList';
 
-describe('RestaurantList', () => {
-  it('loads restaurants on first render', () => {
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
-    const restaurants = [];
+import {rest} from 'msw';
+import {setupServer} from 'msw/node';
+import {act} from 'react-dom/test-utils';
 
-    render(
-      <RestaurantList
-        loadRestaurants={loadRestaurants}
-        restaurants={restaurants}
-      />,
+describe('RestaurantList', () => {
+  const restaurantsUrl =
+    'https://api.outsidein.dev/nkt25RaVlQOPIXq6iEZ6a8pbJrgvzqYv/restaurants';
+
+  const restaurantsResponse = rest.get(restaurantsUrl, (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {id: 1, name: 'Sushi Place'},
+        {id: 2, name: 'Pizza Place'},
+      ]),
     );
-    expect(loadRestaurants).toHaveBeenCalled();
   });
 
-  it('displays the restaurants', () => {
-    const noop = () => {};
-    const restaurants = [
-      {id: 1, name: 'Sushi Place'},
-      {id: 2, name: 'Pizza Place'},
-    ];
+  const restaurantsErrorResponse = rest.get(restaurantsUrl, (req, res, ctx) => {
+    return res(ctx.status(500));
+  });
 
-    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />);
+  const handlers = [restaurantsResponse];
 
-    expect(screen.getByText('Sushi Place')).toBeInTheDocument;
-    expect(screen.getByText('Pizza Place')).toBeInTheDocument;
+  const server = new setupServer(...handlers);
+
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  it('displays the restaurants', async () => {
+    render(<RestaurantList />);
+
+    await waitFor(() => {
+      expect(screen.getByText('Sushi Place')).toBeInTheDocument;
+      expect(screen.getByText('Pizza Place')).toBeInTheDocument;
+    });
+  });
+
+  it('Shows error message when api response with an error', async () => {
+    server.use(restaurantsErrorResponse);
+    render(<RestaurantList />);
+    await waitFor(() => {
+      const errorMessage = screen.getByText('Error, intentelo más tarde');
+      expect(errorMessage).toBeVisible;
+    });
   });
 });
--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -1,11 +1,10 @@
 import RestaurantList from './RestaurantList';
-import {useRestaurants} from './useRestaurants';
 
 export default function RestaurantScreen() {
   return (
     <div>
       <h1>Restaurants</h1>
-      <RestaurantList useRestaurants={useRestaurants} />
+      <RestaurantList />
     </div>
   );
 }
